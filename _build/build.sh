791a54f60193a1481aa0a2b21597cc9710df8d20
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-05-29 17:22:28 -0700

    ignore github commit history links

--- _build/build.sh
+++ _build/build.sh
@@ -3,5 +3,5 @@
 bundle exec jekyll build && bundle exec htmlproofer ./_site \
   --assume-extension \
   --timeframe '30d' \
-  --http-status-ignore 0,503
-  
+  --http-status-ignore 0,503 \
+  --url-ignore /https://github.com/mdlincoln/mdlincoln.github.io/commits/*/
