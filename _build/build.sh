49a8eea25b8519e5b3c376be593187925b894948
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-12-02 10:34:44 -0500

    ignore 429 errors

--- _build/build.sh
+++ _build/build.sh
@@ -3,5 +3,5 @@
 bundle exec jekyll build && bundle exec htmlproofer ./_site \
   --assume-extension \
   --timeframe '30d' \
-  --http-status-ignore 0,503,302,301,303,403 \
+  --http-status-ignore 0,503,302,301,303,403,429 \
   --url-ignore /https://github.com/mdlincoln/mdlincoln.github.io/commits/*/,/collection.britishmuseum.org/
