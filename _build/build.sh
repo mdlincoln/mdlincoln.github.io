ce994b82b751d19b35165c5fe92b16174625d042
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2021-03-14 15:06:08 -0400

    ignore 500 errors

--- _build/build.sh
+++ _build/build.sh
@@ -3,5 +3,5 @@
 bundle exec jekyll build && bundle exec htmlproofer ./_site \
   --assume-extension \
   --timeframe '30d' \
-  --http-status-ignore 0,503,302,301,303,403,429 \
+  --http-status-ignore 0,500,503,302,301,303,403,429 \
   --url-ignore /https://github.com/mdlincoln/mdlincoln.github.io/commits/*/,/collection.britishmuseum.org/,/https?://twitter.com*/,/https?://t.co*/,/https://cards-dev.twitter.com/validator/
