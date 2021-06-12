616956269c431b77907470e997472800936da98b
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2021-06-12 08:09:03 -0400

    don't check link for google fonts preconnect

--- _build/build.sh
+++ _build/build.sh
@@ -4,4 +4,4 @@ bundle exec jekyll build && bundle exec htmlproofer ./_site \
   --assume-extension \
   --timeframe '30d' \
   --http-status-ignore 0,500,503,502,302,301,303,403,429 \
-  --url-ignore /https://github.com/mdlincoln/mdlincoln.github.io/commits/*/,/collection.britishmuseum.org/,/https?://twitter.com*/,/https?://t.co*/,/https://cards-dev.twitter.com/validator/
+  --url-ignore /https://github.com/mdlincoln/mdlincoln.github.io/commits/*/,/collection.britishmuseum.org/,/https?://twitter.com*/,/https?://t.co*/,/https://cards-dev.twitter.com/validator/,https://fonts.gstatic.com
